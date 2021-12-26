844ec6f7d2e6e3215b7029464da17b1e2fbc1676
Álvaro <agonzalez1554@gmail.com>
2021-12-26 01:06:29 -0300

    CRUD departamentos y edificios

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -60,6 +60,7 @@ class ApartmentsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_apartment
       @apartment = Apartment.find(params[:id])
+      @buildings = Building.all
     end
 
     # Only allow a list of trusted parameters through.
